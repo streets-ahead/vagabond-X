b73b1f4a96aeac7872776b5985242b76175b4afb
Sam Mussell <smussell@apple.com>
2014-03-25 10:40:58 -0700

    couple tweaks

--- src/vagabond/core.clj
+++ src/vagabond/core.clj
@@ -2,28 +2,29 @@
   (:require [liberator.core :refer [resource defresource by-method]]
     [liberator.representation :refer [ring-response]]
     [ring.middleware.params :refer [wrap-params]]
-    [ring.adapter.jetty :refer [run-jetty]]      
+    [ring.adapter.jetty :refer [run-jetty]]
     [compojure.core :refer [defroutes ANY GET POST]]
     [vagabond.blog_service :as blog_service]
     [ring.middleware.json :refer [wrap-json-body]]))
 
+
 (defn- create-exists [get-data]
-  (fn [ctx] 
+  (fn [ctx]
     (let [data (get-data)]
       [(not (empty? data)) {::req-data data}])))
 
-(defn- handle-conflict [{conflict ::conflict}] 
+(defn- handle-conflict [{conflict ::conflict}]
   (when conflict
     (ring-response {:status 409 :body "duplicate title!"})))
 
 (defn- check-conflict [{conflict ::conflict}] (not conflict))
 
 (defn- create-modifier [action]
-  (fn [ctx] 
+  (fn [ctx]
     (let [body (get-in ctx [:request :body])
-          post (assoc body :author (get-in ctx [::user :id]))] 
+          post (assoc body :author (get-in ctx [::user :id]))]
       (when-not (action post) {::conflict true}))))
-  
+
 (def route-defaults {
   :available-media-types ["text/html" "application/json"]
   :handle-ok ::req-data
@@ -40,9 +41,9 @@
   :allowed? (fn [ctx] {::user {:id 1}})
   :new? check-conflict
   :post! (create-modifier #(blog_service/create-post %)))
-  
+
 ; handler for all things /[slug]
-(defresource single-post [slug] route-defaults 
+(defresource single-post [slug] route-defaults
   :allowed-methods [:put :get :delete]
   :allowed? (fn [ctx] (by-method {:get true
                                   :any (fn [ctx] {::user {:id 1}})}))
@@ -62,7 +63,7 @@
   :allowed-methods [:post]
   :available-media-types ["application/json"]
   :new? false
-  :allowed? (fn [ctx] 
+  :allowed? (fn [ctx]
                 (let [args (select-values (get-in ctx [:request :body]) [:username :password])
                       user (apply blog_service/auth-user args)]
                   [user, {::user user}]))
@@ -77,9 +78,6 @@
   (ANY "/posts/author/:author" [author] (by-author author))
   (ANY "/authenticate" [] auth-resource))
 
-(def handler (wrap-json-body app {:keywords? true}))  
+(def handler (wrap-json-body app {:keywords? true}))
 
 (defn -main [] (run-jetty #'handler {:port 3000}))
-
-
-
